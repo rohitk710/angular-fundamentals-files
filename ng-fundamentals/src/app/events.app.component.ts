45bd0c928c2615dbec1b379ad7cfac9f6e16eca9
Rohit <rohit.pandey@uipath.com>
2020-06-28 23:46:01 +0530

    1st module

---
+++ ng-fundamentals/src/app/events.app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+  <h2>Hello World</h2> <div> </div>
+  <img src="/assets/images/basic-shield.png">
+  `,
+})
+export class EventsAppComponent {
+  title = 'Events';
+}
